e7e0273c71f5aee73173d344cb1b944d1cff96e7
Derek Williams <Williamsdj503@gmail.com>
2025-01-14 10:58:26 -0700

    note to shelf

--- note.sh
+++ note.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+add_notes() {
+    filename=$1
+
+    read -p "what is the name of the file:" filename
+    read -p "Enter the note you want to add:" newnote
+
+
+    if [ "$newnote" != "" ]; then
+        echo $newnote >> $filename
+    fi
+}
+add_notes 
